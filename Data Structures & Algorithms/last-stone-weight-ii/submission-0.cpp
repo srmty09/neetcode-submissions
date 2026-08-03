cf9f91f384d38616908087210c664dd6888e4d0a
Smruti Ranjan Mohanty <smruti45ranjan@gmail.com>
2026-08-03 11:58:31 +0530

    Add: last-stone-weight-ii - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight-ii/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int lastStoneWeightII(vector<int>& stones) {
+        int total = 0;
+        for (int x : stones) total += x;
+
+        int target = total / 2;
+
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
+
+        for (int stone : stones) {
+            for (int j = target; j >= stone; j--) {
+                dp[j] = dp[j] || dp[j - stone];
+            }
+        }
+
+        for (int j = target; j >= 0; j--) {
+            if (dp[j]) {
+                return total - 2 * j;
+            }
+        }
+
+        return 0;
+    }
+};
